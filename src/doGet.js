cb0221db7a8513248b241d55833c95965ba5d5ca
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-01-10 15:07:19 +0900

    feat: 🎸 ウェブアプリ（ウェブページ）で一般ユーザの閲覧制限をかける方法とフォーマットを追加した

--- src/doGet.js
+++ src/doGet.js
@@ -1,8 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function doGet() {
-  return HtmlService.createHtmlOutputFromFile("index")
-    .addMetaTag("viewport", "width=device-width, initial-scale=1")
-    .setFaviconUrl(
-      "https://www.google.co.jp/images/branding/googleg/1x/googleg_standard_color_128dp.png"
-    )
-    .setTitle("ページタイトル");
+  // eslint-disable-next-line no-undef
+  const activeUserEmail = Session.getActiveUser().getEmail()
+
+  // eslint-disable-next-line no-undef
+  console.log(Session.getActiveUser().getEmail()) // hogefuga@gmail.com
+  // eslint-disable-next-line no-undef
+  console.log(Session.getActiveUser().getUsername()) // hogefuga
+
+  if (['ok@example.com', 'good@example.com'].includes(activeUserEmail)) {
+    // eslint-disable-next-line no-undef
+    return HtmlService.createHtmlOutputFromFile('index')
+      .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+      .setFaviconUrl(
+        'https://www.google.co.jp/images/branding/googleg/1x/googleg_standard_color_128dp.png'
+      )
+      .setTitle('ページタイトル')
+  } else {
+    // eslint-disable-next-line no-undef
+    return HtmlService.createHtmlOutputFromFile('404')
+  }
 }
